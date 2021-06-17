aeaab3035c25f2a9a97fbaf6369021cf7c8e5522
SunwoongShin <dev.ios.edward@gmail.com>
2021-06-17 21:35:24 +0900

    a

--- server/routes/dialogflow.js
+++ server/routes/dialogflow.js
@@ -10,10 +10,10 @@ const projectId = config.googleProjectID
 const sessionId = config.dialogFlowSessionID
 const languageCode = config.dialogFlowSessionLanguageCode
 
-
 // Create a new session
 const sessionClient = new dialogflow.SessionsClient();
 const sessionPath = sessionClient.sessionPath(projectId, sessionId);
+
 
 // We will make two routes 
 
@@ -74,7 +74,40 @@ router.post('/eventQuery', async (req, res) => {
     res.send(result)
 })
 
+// Text Query Router
 
+router.post('/textQuery', async (req, res) => {
+
+    // to send some information that comes from the client to Dialogflow 
+    
+      // The text query request.
+      const request = {
+        session: sessionPath,
+        queryInput: {
+          text: {
+            // The query to send to the dialogflow agent
+            text: req.body.text,
+            // The language used by the client (en-US)
+            languageCode: languageCode,
+          },
+        },
+      };
+     
+      // Send request and log result
+      const responses = await sessionClient.detectIntent(request);
+      console.log('Detected intent');
+      const result = responses[0].queryResult;
+      console.log(`  Query: ${result.queryText}`);
+      console.log(`  Response: ${result.fulfillmentText}`);
+      
+      res.send(result)
+    }
+
+
+
+,)
+
+// Event Query Router
 
 
 
